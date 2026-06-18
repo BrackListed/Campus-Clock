b92a1c19b0bbcf37396ad42553e47e9e230aa72f
BrackListed <cameta8900@gmail.com>
2026-06-18 18:26:38 +0800

    Header done with logo text and placeholder pfp

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,17 +1,26 @@
-import { Text, View } from "react-native";
-import "../global.css"
- 
+import { Image, Text, View } from "react-native";
+import "../global.css";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text className="text-2xl border-2 border-green-700">Hello, World Hey!</Text>
+    <View className="flex-1 bg-slate-950">
+      <View className="bg-slate-800/70 h-28 rounded-t-lg flex flex-row items-center justify-between border-green-700 border-2">
+        <View className="flex-row gap-2 px-4 py-3 h-full border-zinc-50 border-2 w-1/2 items-center">
+          <Image
+            source={require("../assets/images/Campus-Clock-logo.png")}
+            className="w-12 h-full"
+            resizeMode="contain"
+          />
+          <Text className="text-zinc-50 font-semibold">CAMPUS CLOCK</Text>
+        </View>
+        <View className="flex-row gap-3 flex justify-end px-4 py-3">
+          <Image
+            source={require("../assets/images/esquie.jpg")}
+            className="w-10 h-10 rounded-full"
+            resizeMode="contain"
+          />
+        </View>
+      </View>
     </View>
   );
 }
