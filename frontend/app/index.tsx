73d59c9c267529e1cdfe7214f5e14db5cda8305b
BrackListed <cameta8900@gmail.com>
2026-06-19 17:49:21 +0800

    Finished the Add new assignment interface

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,5 +1,5 @@
-import { Bell } from "lucide-react-native";
-import { Image, Pressable, Text, View } from "react-native";
+import { AlarmClock, Bell, Calendar, ChevronDown } from "lucide-react-native";
+import { Image, Pressable, Text, TextInput, View } from "react-native";
 import "../global.css";
 
 export default function Index() {
@@ -23,8 +23,8 @@ export default function Index() {
           <Bell fill={"white"} color={"white"} />
         </View>
       </View>
-      <View className="flex-1 flex-col flex px-3 py-2 text-zinc-50 overflow-y-auto">
-        <View className="bg-slate-800/70 border-2 border-green-50 h-fit p-2 flex flex-col gap-1">
+      <View className="flex-col flex px-3 py-2 text-zinc-50 overflow-y-auto">
+        <View className="bg-slate-800/70 rounded-lg border border-white/10 h-fit p-2 flex flex-col gap-1">
           <Text className="text-zinc-50 font-bold text-lg">
             School Commute Tracker
           </Text>
@@ -46,13 +46,63 @@ export default function Index() {
             </View>
           </View>
           <View className="flex flex-row gap-5 items-center justify-between">
-            <Pressable className="bg-lime-300 px-4 py-2 rounded-lg">
+            <Pressable className="bg-lime-300 px-4 py-2 rounded-lg active:bg-lime-400 ">
               <Text className="font-semibold">Stop & Save</Text>
             </Pressable>
             <View className="flex flex-row gap-1">
               <Text className="text-zinc-50">Weekly Avg:</Text>
               <Text className="text-lime-300">0 Mins</Text>
             </View>
+          </View>
+        </View>
+      </View>
+      <View className="flex-col flex px-3 py-2 text-zinc-50">
+        <View className="bg-slate-800/70 rounded-lg border border-white/10 h-fit p-2 flex flex-col gap-3">
+          <Text className="text-zinc-50 font-bold text-lg">
+            Add New Assignment
+          </Text>
+          <TextInput
+            className="bg-white rounded-lg p-2"
+            placeholder="Assignment Title(e.g., Math Homework)"
+          />
+          <View className="flex flex-col gap-2">
+            <View className="flex flex-row w-full gap-2">
+              <View className="flex-1 border border-white/10 rounded-lg flex flex-col justify-center p-2">
+                <Text className="text-gray-400">Due Date</Text>
+                <View className="flex flex-row items-center gap-2 rounded-lg">
+                  <Calendar color={"gray"} size={"15"} />
+                  <Text className="text-zinc-50">Jan 01</Text>
+                </View>
+              </View>
+              <View className="flex-1 border border-white/10 rounded-lg flex flex-col justify-center p-2">
+                <Text className="text-gray-400">Subject</Text>
+                <View className="flex flex-row items-center gap-2 rounded-lg">
+                  <Text className="text-zinc-50">Physics</Text>
+                  <ChevronDown color={"gray"} size={15} />
+                </View>
+              </View>
+            </View>
+            <View className="flex flex-row w-full gap-2">
+              <View className="flex-1 border border-white/10 rounded-lg flex flex-col justify-center p-2">
+                <Text className="text-gray-400">Reminder Time</Text>
+                <View className="flex flex-row items-center gap-2 rounded-lg">
+                  <AlarmClock color={"gray"} size={"15"} />
+                  <Text className="text-zinc-50">3:00 PM</Text>
+                </View>
+              </View>
+              <View className="flex-1 border border-white/10 rounded-lg flex flex-col justify-center p-2">
+                <Text className="text-gray-400">Priority</Text>
+                <View className="flex flex-row items-center gap-2 rounded-lg">
+                  <Text className="text-zinc-50">Low</Text>
+                  <ChevronDown color={"gray"} size={15} />
+                </View>
+              </View>
+            </View>
+            <Pressable className="rounded-lg bg-cyan-300 flex items-center justify-center h-10">
+              <Text className="font-semibold">
+                Create Assignment & Set Alarm
+              </Text>
+            </Pressable>
           </View>
         </View>
       </View>
