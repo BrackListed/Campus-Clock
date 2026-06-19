7925843c23ef8e56241c5ec11129b134fde82f71
BrackListed <cameta8900@gmail.com>
2026-06-19 16:19:30 +0800

    Lucide in react native imported

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,11 +1,12 @@
+import { Bell } from "lucide-react-native";
 import { Image, Text, View } from "react-native";
 import "../global.css";
 
 export default function Index() {
   return (
     <View className="flex-1 bg-slate-950">
-      <View className="bg-slate-800/70 h-28 rounded-t-lg flex flex-row items-center justify-between border-green-700 border-2">
-        <View className="flex-row gap-2 px-4 py-3 h-full border-zinc-50 border-2 w-1/2 items-center">
+      <View className="bg-slate-800/70 h-28 rounded-t-lg flex flex-row items-center justify-between">
+        <View className="flex-row gap-2 px-4 py-3 h-full w-1/2 items-center">
           <Image
             source={require("../assets/images/Campus-Clock-logo.png")}
             className="w-12 h-full"
@@ -13,12 +14,13 @@ export default function Index() {
           />
           <Text className="text-zinc-50 font-semibold">CAMPUS CLOCK</Text>
         </View>
-        <View className="flex-row gap-3 flex justify-end px-4 py-3">
+        <View className="flex-row gap-3 flex justify-end px-4 py-3 items-center text-zinc-50">
           <Image
             source={require("../assets/images/esquie.jpg")}
             className="w-10 h-10 rounded-full"
             resizeMode="contain"
           />
+          <Bell fill={"white"} color={"white"} />
         </View>
       </View>
     </View>
